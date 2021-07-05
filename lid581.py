3500f57350172dc0f0ab3ae16028bed45e644ff0
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-05 10:18:33 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -904,7 +904,7 @@ def balancer_semanal():
 def balancer_diario_proc():
 
 	with st.beta_expander('Pontos'):
-		st.image('balancer_diario/Pontos diario balancer.jpg')
+		st.image('balancer_diario/Pontos diario balancer.JPG')
 
 	with st.beta_expander('Procedimentos folha 1'):
 		st.image('balancer_semanal/folha1.jpg')
