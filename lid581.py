1469498df0b082818bdbd4c12473754ce737c039
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-12 15:24:44 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -1769,7 +1769,7 @@ def trouble_shell():
 
 	st.subheader('Identificando o problema')
 
-	nv1 = st.selectbox('1) Qual o problema?', df['Nv1'].unique() , index=0, key='shell1')
+	nv1 = st.selectbox('1) Qual o problema?', df['Nv1'].unique() , key='shell1')
 	df_nv1 = df[df['Nv1'] == nv1]
 	
 	if df_nv1.shape[0] > 0:
@@ -1781,10 +1781,10 @@ def trouble_shell():
 		if df_nv2.shape[0] > 0:
 			_st3, _st4 = st.beta_columns(2)
 
-			causa = _st3.radio('3) Causa', df_nv2['Causa'].unique(), index=0, key='shell3')
+			causa = _st3.radio('3) Causa', df_nv2['Causa'].unique(), key='shell3')
 			df_causa = df_nv2[df_nv2['Causa'] == causa]
 
-			solucao = _st4.radio('4) Solução', df_nv2['Solucao'].unique(), index=0, key='shell4')
+			solucao = _st4.radio('4) Solução', df_nv2['Solucao'].unique(), key='shell4')
 		
 	with st.form('Form'):
 		s1, s2,  = st.beta_columns([2,8])
