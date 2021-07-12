b3ba0c0a773598cf44de31d6197e8725c3a9a572
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-12 14:12:46 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -1777,7 +1777,7 @@ def trouble_shell():
 	st.subheader('Avaliando causa e solução')
 	_st3, _st4 = st.beta_columns(2)
 
-	nv1 = _st1.radio('1) Qual o problema?', df['Nv1'].unique(), index=0, key='1', on_change= main())
+	nv1 = _st1.radio('1) Qual o problema?', df['Nv1'].unique(), index=0, key='1', on_change = break)
 	df_nv1 = df[df['Nv1'] == nv1]
 
 	nv2 = _st2.radio('2) Qual o problema?', df_nv1['Nv2'].unique(), index=0,  key='2')
