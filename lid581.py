ca6d945c940da9dc7a2c7405c8a944e88e4c7f6e
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-12 13:44:37 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -1779,15 +1779,24 @@ def trouble_shell():
 
 	nv1 = _st1.radio('1) Qual o problema?', df['Nv1'].unique(), index=0, key='1')
 	df_nv1 = df[df['Nv1'] == nv1]
-
-	nv2 = _st2.radio('2) Qual o problema?', df_nv1['Nv2'].unique(), index=0,  key='2')
-	df_nv2 = df_nv1[df_nv1['Nv2'] == nv2]
-
-	causa = _st3.radio('3) Causa', df_nv2['Causa'].unique(), index=0, key='3')
-	df_causa = df_nv2[df_nv2['Causa'] == causa]
-
-	solucao = _st4.radio('4) Solução', df_nv2['Solucao'].unique(), index=0, key='4')
 	
+	try:
+		nv2 = _st2.radio('2) Qual o problema?', df_nv1['Nv2'].unique(), index=0,  key='2')
+		df_nv2 = df_nv1[df_nv1['Nv2'] == nv2]
+	except:
+		st.error('Tente novamente')
+	
+	try:
+		causa = _st3.radio('3) Causa', df_nv2['Causa'].unique(), index=0, key='3')
+		df_causa = df_nv2[df_nv2['Causa'] == causa]
+	except:
+		st.error('Tente novamente')
+	
+	try:
+		solucao = _st4.radio('4) Solução', df_nv2['Solucao'].unique(), index=0, key='4')
+	except:
+		st.error('Tente novamente')
+		
 	with st.form('Form'):
 		s1, s2,  = st.beta_columns([2,8])
 		s3, s4, s5 = st.beta_columns([2, 2, 6])
