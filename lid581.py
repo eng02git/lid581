b7b6a2f26cfbe2ef1f053296b13a26f2df63e540
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-12 15:34:49 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -1765,25 +1765,7 @@ def test():
 	
 
 def trouble_shell():
-	df = pd.read_csv("troubleshoot_csv/shell.csv", sep=';')
 
-	st.subheader('Identificando o problema')
-
-	nv1 = st.selectbox('1) Qual o problema?', list(df['Nv1'].unique()) , key='shell1')
-	df_nv1 = df[df['Nv1'] == nv1]
-	
-	if df_nv1.shape[0] > 0:
-		nv2 = st.selectbox('2) Qual o problema?', list(df_nv1['Nv2'].unique()),  key='shell2')
-		df_nv2 = df_nv1[df_nv1['Nv2'] == nv2]
-		
-		st.subheader('Avaliando causa e solução')
-		if df_nv2.shape[0] > 0:
-			_st3, _st4 = st.beta_columns(2)
-
-			causa = _st3.radio('3) Causa', list(df_nv2['Causa'].unique()), key='shell3')
-			df_causa = df_nv2[df_nv2['Causa'] == causa]
-
-			solucao = _st4.radio('4) Solução', list(df_nv2['Solucao'].unique()), key='shell4')
 	'''	
 	with st.form('Form'):
 		s1, s2,  = st.beta_columns([2,8])
@@ -2046,6 +2028,25 @@ if __name__ == '__main__':
 	if func_escolhida == 'Shell Press':
 		st.subheader('Troubleshooting Shell Press')
 		trouble_shell()
+		df = pd.read_csv("troubleshoot_csv/shell.csv", sep=';')
+
+		st.subheader('Identificando o problema')
+
+		nv1 = st.selectbox('1) Qual o problema?', list(df['Nv1'].unique()) , key='shell1')
+		df_nv1 = df[df['Nv1'] == nv1]
+
+		if df_nv1.shape[0] > 0:
+			nv2 = st.selectbox('2) Qual o problema?', list(df_nv1['Nv2'].unique()),  key='shell2')
+			df_nv2 = df_nv1[df_nv1['Nv2'] == nv2]
+
+			st.subheader('Avaliando causa e solução')
+			if df_nv2.shape[0] > 0:
+				_st3, _st4 = st.beta_columns(2)
+
+				causa = _st3.radio('3) Causa', list(df_nv2['Causa'].unique()), key='shell3')
+				df_causa = df_nv2[df_nv2['Causa'] == causa]
+
+				solucao = _st4.radio('4) Solução', list(df_nv2['Solucao'].unique()), key='shell4')
 		
 	if func_escolhida == 'Autobagger':
 		st.subheader('Troubleshooting Autobagger')
