4b3f5d16215cb347f9440668fb50c1da8cf5e148
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-12 14:32:56 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -1777,25 +1777,22 @@ def trouble_shell():
 	dic = {}
 
 	st.subheader('Identificando o problema')
-	_st1, _st2 = st.beta_columns(2)
-	st.subheader('Avaliando causa e solução')
-	_st3, _st4 = st.beta_columns(2)
 
-	nv1 = _st1.selectbox('1) Qual o problema?', df['Nv1'].unique() , index=0, key='1')
+	nv1 = st.selectbox('1) Qual o problema?', df['Nv1'].unique() , index=0, key='1')
 	#nv1 = _st1.radio('1) Qual o problema?', df['Nv1'].unique(), index=0, key='1')
 	df_nv1 = df[df['Nv1'] == nv1]
 	
-	nv2 = _st2.selectbox('2) Qual o problema?', df_nv1['Nv2'].unique(), index=0,  key='2')
+	nv2 = st.selectbox('2) Qual o problema?', df_nv1['Nv2'].unique(), index=0,  key='2')
 	#nv2 = _st2.radio('2) Qual o problema?', df_nv1['Nv2'].unique(), index=0,  key='2')
 	df_nv2 = df_nv1[df_nv1['Nv2'] == nv2]
-
+	
+	st.subheader('Avaliando causa e solução')
+	_st3, _st4 = st.beta_columns(2)
+	
 	causa = _st3.radio('3) Causa', df_nv2['Causa'].unique(), index=0, key='3')
 	df_causa = df_nv2[df_nv2['Causa'] == causa]
 
 	solucao = _st4.radio('4) Solução', df_nv2['Solucao'].unique(), index=0, key='4')
-
-	st.error('Tente novamente')
-
 		
 	with st.form('Form'):
 		s1, s2,  = st.beta_columns([2,8])
