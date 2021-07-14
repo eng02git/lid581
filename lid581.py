47acb3f73d1d57fdb4cc4ea53dc8ca21b8100eec
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-14 13:55:58 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -2794,8 +2794,9 @@ if __name__ == '__main__':
 		df_cil = load_forms_cil('Liner_diario')
 		liner_s = df_cil.copy()
 		liner_s['I2'] = liner_s['I2'].dt.date
+		liner_s['Semana'] = liner_s['I2'].dt.strftime('%G-%V')
 		liner_s = liner_s.rename(columns={'I2': 'Datas'})
-		liner_s['Semana'] = liner_s['Datas'].dt.strftime('%G-%V')
+		#liner_s['Semana'] = liner_s['Datas'].dt.strftime('%G-%V')
 		liner_s.drop_duplicates(subset=['Datas'])
 		liner_s = liner_s.replace({'NOK':0, 'OK':1})
 		#liner_s['Liner'] = round((liner_d['Q00'] + liner_d['Q01'] + liner_d['Q02'] + liner_d['Q03'] + liner_d['Q04'] + liner_d['Q05'] + liner_d['Q06'] + liner_d['Q07'] + liner_d['Q08'])*100/9, 2)
