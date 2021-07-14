a8ff1376d0b1e694033addab727c292212a6efed
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-14 11:26:47 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -2733,7 +2733,7 @@ if __name__ == '__main__':
 		cil_diario['Datas'] = pd.date_range(start=inicio_filtro, end=fim_filtro)
 		cil_diario['Datas'] = cil_diario['Datas'].dt.date
 		
-		cil_teste = pd.concat([cil_diario, df_cil_auto_dia])
+		cil_teste = pd.concat([cil_diario, df_cil_auto_dia], axis=1)
 		cil_teste = cil_teste.replace(np.nan, '-', regex=True)
 		
 		gridOptions, grid_height, return_mode_value, update_mode_value, fit_columns_on_grid_load, enable_enterprise_modules = config_grid(cil_teste)
