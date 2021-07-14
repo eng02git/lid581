614e71118ac8e77d8644d89bfef387d375d0328b
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-14 11:17:52 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -20,6 +20,7 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 import json
 import smtplib
 from datetime import  date, datetime, time
