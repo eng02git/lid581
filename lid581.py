b29cdff9d77e5c285240cd8f3f1932aaaddb7646
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-14 16:01:48 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -2730,11 +2730,12 @@ if __name__ == '__main__':
 		liner_d = liner_d.rename(columns={'I2': 'Datas'})
 		st.write(liner_d)
 		liner_d.drop_duplicates(subset=['Datas'])
-		st.write(liner_d)
+		
 		liner_d = liner_d.replace({'NOK':0, 'OK':1})
 		liner_d['Liner'] = round((liner_d['Q00'] + liner_d['Q01'] + liner_d['Q02'] + liner_d['Q03'] + liner_d['Q04'] + liner_d['Q05'] + liner_d['Q06'] + liner_d['Q07'] + liner_d['Q08'])*100/9, 2)
-		liner_d = liner_d.groupby('A', group_keys=False).mean()
+		liner_d = liner_d.groupby('Datas', group_keys=False).mean()
 		#liner_s = liner_s.groupby(['Semanas']).mean()
+		st.write(liner_d)
 		
 		# Shell diario
 		df_cil = load_forms_cil('shell_diario')
