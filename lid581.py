5dee427d5dc3f954b3b2f60a263fa5453dbf26bc
eng01git <84606447+eng01git@users.noreply.github.com>
2021-07-15 10:30:08 -0300

    Update lid581.py

--- lid581.py
+++ lid581.py
@@ -3142,7 +3142,7 @@ if __name__ == '__main__':
 			fig.add_trace(go.Bar(x=['Liner' ,'Shell', 'Autobagger', 'Conversion', 'Balancer'], y=grafico_d.sum(), marker=dict(color='rgba(12, 50, 196, 0.6)')), row=1, col=1)
 			#go.Bar(x=animals, y=[20, 14, 23])
 			fig.add_trace(go.Histogram(x=response['data'], marker=dict(color='rgba(12, 50, 196, 0.6)')), row=1, col=2)
-			fig.update_layout(height=300, width=500,showlegend=False)
+			fig.update_layout(showlegend=False)
 			col_d.write(fig)
 			
 			if selected != []:
